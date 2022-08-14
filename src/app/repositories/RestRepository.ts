18605d55152b0676c1e2915017225f622b210397
Natalia Gherardi <natirgh@gmail.com>
2022-08-14 18:30:03 +0200

    added cors to get method

--- src/app/repositories/RestRepository.ts
+++ src/app/repositories/RestRepository.ts
@@ -4,7 +4,7 @@ class RestRepository<T extends Response> implements IRestRepository<T> {
   constructor(public url: string) {}
 
   getAll() {
-    return fetch(this.url)
+    return fetch(this.url, { mode: "cors" })
       .then((response) => response.json())
       .catch((error) => error);
   }
